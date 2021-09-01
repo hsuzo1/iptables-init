2dfd22ebfe83213f5bda1c90909d582bacb6fa91
blog <email@email.com>
2021-09-01 09:27:44 +0800

    modi

--- iptables-config.sh
+++ iptables-config.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+PUB_IF=$(ip a| grep "BROADCAST" | awk -F ":" 'NR==1{print $2}' | sed "s/ //g")
+
 # Empty all rules
 sudo iptables -t filter -F
 sudo iptables -t filter -X
@@ -14,6 +17,12 @@ sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -t filter -A INPUT -i lo -j ACCEPT
 sudo iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
+# Block sync
+# sudo iptables -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP
+ 
+# Block Fragments
+# sudo iptables -A INPUT -i ${PUB_IF} -f -j DROP
+
 # ICMP (Ping)
 sudo iptables -t filter -A INPUT -p icmp -j ACCEPT
 
@@ -22,27 +31,12 @@ sudo iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -t filter -A INPUT -p tcp --dport 1696 -j ACCEPT
 
 # DNS
-sudo iptables -t filter -A INPUT -p tcp --dport 53 -j ACCEPT
-sudo iptables -t filter -A INPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -t filter -A INPUT -p tcp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t filter -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 # HTTP HTTPS
 sudo iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -t filter -A INPUT -p tcp --dport 443 -j ACCEPT
-
-# FTP
-#sudo iptables -t filter -A INPUT -p tcp --dport 20:21 -j ACCEPT
-
-# Git
-#sudo iptables -t filter -A INPUT -p tcp --dport 9418 -j ACCEPT
-
-# Mail SMTP
-#iptables -t filter -A INPUT -p tcp --dport 25 -j ACCEPT
-
-# Mail POP3
-#iptables -t filter -A INPUT -p tcp --dport 110 -j ACCEPT
-
-# Mail IMAP
-#iptables -t filter -A INPUT -p tcp --dport 143 -j ACCEPT
 
 # NTP (server time)
 sudo iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
@@ -52,12 +46,13 @@ sudo iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
 if [ -e ips.txt ]
 then
 echo "already got ips.txt"
-else wget "http://downloads.sourceforge.net/hibernate/hibernate-3.2.5.ga.zip?modtime=1185893922&big_mirror=1"
+else wget "https://raw.githubusercontent.com/hsuzo1/iptables-init/main/ips.txt"
 fi
 
-# Add Trusted IPs
+# Add All Trusted IPs to iptables
 for ip in `cat ips.txt`
 do
 iptables -I INPUT -s $ip -j ACCEPT
 done
 
+echo "All Done!"
